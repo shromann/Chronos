c484a0073c1a137b133e6d2f453c412a2d1e5a85
Weng Jae Chin <wengjae@gmail.com>
2022-04-08 18:04:36 +1000

    able to query data from firebase - first example: getAllEvents

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
+import getAllEvents from './server/getAllEvents';
 
 function App() {
+  getAllEvents();
   return (
     <div className="App">
       <header className="App-header">
