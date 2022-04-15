2f3de04ccf7c0b4e0fa23d23f00a08d65b436783
Jaee-C <wengjae@gmail.com>
2022-04-15 19:45:47 +1000

    dynamic height

--- src/components/CalendarColumn/EventBox.js
+++ src/components/CalendarColumn/EventBox.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from "@emotion/styled";
 import { Box } from '@mui/material';
 
-import { DAY_CALENDAR_WIDTH, DAY_CALENDAR_HEIGHT } from '../utils/constants';
+import { DAY_CALENDAR_WIDTH } from '../utils/constants';
 import { 
   eventHeight, 
   eventPlacement, 
@@ -30,16 +30,16 @@ const eventTimes = (event) => {
 }
 
 
-const EventBox = ({ event }) => {
+const EventBox = ({ event, maxHeight }) => {
   const duration = getEventDuration(event.start_time, event.end_time);
   console.log(event);
   return <Box
     sx={{
       width: DAY_CALENDAR_WIDTH + 'px',
-      height: eventHeight(duration, DAY_CALENDAR_HEIGHT),
+      height: eventHeight(duration, maxHeight),
       backgroundColor: 'gray',
       position: 'absolute',
-      top: eventPlacement(event.start_time, DAY_CALENDAR_HEIGHT),
+      top: eventPlacement(event.start_time, maxHeight),
       textAlign: 'left',
       // border: '1.5px solid #B9B9B9',
       // opacity: '0.5'
