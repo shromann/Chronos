304baf092c7f21b8ef21735bd499fefcd1b3f52e
Shromann Majumder <shromannmajumder@gmail.com>
2022-04-15 15:11:11 +1000

    consts

--- src/components/utils/constants.js
+++ src/components/utils/constants.js
@@ -1,12 +1,8 @@
 // unit: px
 export const DAY_CALENDAR_WIDTH = 755;
-<<<<<<< HEAD
 
 
 export const DAY_CALENDAR_HEIGHT = 1343.5;
-=======
-export const DAY_CALENDAR_HEIGHT = 1325;
->>>>>>> 4de0601f37bb614d4eaf5e75f335b0875d90eeea
 // export const DAY_CALENDAR_HEIGHT = document.getElementById('dayCalendar').clientHeight;
 
 // unit: mins in 24 hours
