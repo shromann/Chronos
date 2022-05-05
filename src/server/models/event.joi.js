55a1db0d2b80493c0952ea63c93ca01b1d4474d1
qiawen yu <qiawen.yu@student.unimelb.edu.au>
2022-05-05 11:14:22 +1000

    implement add events and get all events from firebase

--- src/server/models/event.joi.js
+++ src/server/models/event.joi.js
@@ -1,8 +1,10 @@
-import Joi from 'joi';
+import Joi from "joi";
 
 const Event = Joi.object({
-  title: Joi.string().required(),
-  event_time: Joi.date(),
+  name: Joi.string().required(),
+  start_time: Joi.date(),
+  end_time: Joi.date(),
+  focus: Joi.number(),
 });
 
 export default Event;
