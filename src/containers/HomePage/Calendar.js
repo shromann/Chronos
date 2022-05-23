655c1a6512c04615af77792f9d16c4e3594f100f
yuqi-zheng <yuqizheng02@gmail.com>
2022-05-23 12:20:04 +1000

    back and forward arrows: constant position upon scroll

--- src/containers/HomePage/Calendar.js
+++ src/containers/HomePage/Calendar.js
@@ -54,9 +54,10 @@ const Calendar = ({ allEvents }) => {
   }
 
   return (
-    <Container>
+    <div>
       <IconButton onClick={handlePrevDay}><ArrowBackIos /></IconButton>
       <IconButton onClick={handleNextDay}><ArrowForwardIos /></IconButton>
+    <Container>
       <Column>
         <CalendarLabel week={week} />
       </Column>
@@ -66,6 +67,7 @@ const Calendar = ({ allEvents }) => {
         <WeekCalendar events={allEvents} height={height} week={week} />
       </Column>
     </Container>
+    </div>
   )
 };
 
